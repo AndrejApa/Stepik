db376172ee3981dae49fd382ef9f9c3d7fb5709d
AndrejApa <81564427+AndrejApa@users.noreply.github.com>
2021-09-10 19:25:28 +0300

    Goroutine

---
+++ HW10.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func work(wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Println("Начали")
+	time.Sleep(time.Second * 2)
+	fmt.Println("Закончили")
+}
+
+func main() {
+	wg := new(sync.WaitGroup)
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go work(wg)
+	}
+
+	wg.Wait()
+	fmt.Println("Горутины завершили выполнение")
+}
